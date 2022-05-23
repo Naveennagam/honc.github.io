6442224f70b4f579e91aa77a4416f6167601e8ab
Naveen Nagam <naveen.n@honc.io>
2022-05-23 13:24:04 +0530

    First Push

---
+++ main.js
@@ -0,0 +1,14 @@
+// var top1 = $('#home').offset().top;
+// var top2 = $('#featuredWork').offset().top;
+// var top3 = $('#caseStudy').offset().top;
+
+// $(document).scroll(function() {
+//   var scrollPos = $(document).scrollTop();
+//   if (scrollPos >= top1 && scrollPos < top2) {
+//     $('#change').css('background-color', '#f00');
+//   } else if (scrollPos >= top2 && scrollPos < top3) {
+//     $('#change').css('background-color', '#0f0');
+//   } else if (scrollPos >= top3) {
+//     $('#change').css('background-color', '#00f');
+//   }
+// });
