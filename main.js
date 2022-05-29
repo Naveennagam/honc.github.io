a95fc972816dfb1851338a459a86d6b98801db83
Naveen Nagam <naveen.n@honc.io>
2022-05-29 18:13:30 +0530

    changes done to navbar colors scrolling

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-// var top1 = $('#home').offset().top;
-// var top2 = $('#featuredWork').offset().top;
-// var top3 = $('#caseStudy').offset().top;
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        var s1 = $("#s1").height() +130;
+        var s2 = $("#s2").height();
+        var s3 = $("#s3").height();
+        var s4 = $("#s4").height();
+        var s5 = $("#s5").height();
+        var s6 = $("#s6").height();
+       
+        if (scroll > (s1+s2+s3+s4+s5+s6))  {
+            $("nav").css("background" , "#000");
+        }else if (scroll >  (s1+s2+s3+s4+s5)) {
+          $("nav").css("background" , "#F26868");
+        }else if (scroll >  (s1+s2+s3+s4)) {
+            $("nav").css("background" , "#000");
+        }else if (scroll >  (s1+s2+s3)) {
+            $("nav").css("background" , "#F26868");
+        }else if (scroll >  (s1+s2)) {
+            $("nav").css("background" , "#000");
+        }else if (scroll >  (s1)) {
+            $("nav").css("background" , "#F26868");
+        } else{
+            $("nav").css("background" , "#000");  	
+        }
+    })
+  })
 
-// $(document).scroll(function() {
-//   var scrollPos = $(document).scrollTop();
-//   if (scrollPos >= top1 && scrollPos < top2) {
-//     $('#change').css('background-color', '#f00');
-//   } else if (scrollPos >= top2 && scrollPos < top3) {
-//     $('#change').css('background-color', '#0f0');
-//   } else if (scrollPos >= top3) {
-//     $('#change').css('background-color', '#00f');
-//   }
-// });
+  $(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        var se1 = $("#se1").height() - 70;
+        var se2 = $("#se2").height() ;
+       
+        if (scroll >  (se1)) {
+            $("nav").css("background" , " #F26868");
+        } 
+    })
+  })
