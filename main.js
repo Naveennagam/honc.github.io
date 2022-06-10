9d50699e0c04ccedd123f3ca5372bfef5f33e9f8
Naveen Nagam <naveen.n@honc.io>
2022-06-10 17:15:38 +0530

    Initial commit

--- main.js
+++ main.js
@@ -38,6 +38,57 @@ $(document).ready(function(){
     })
   })
   
+  $(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[980,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:true,
+        navigationText:["<",">"],
+        autoPlay:true
+    });
+});
+
+
+  const delay = 3000; //ms
+
+  const slides = document.querySelector(".slides");
+  const slidesCount = slides.childElementCount;
+  const maxLeft = (slidesCount - 1) * 100 * -1;
+  
+  let current = 0;
+  
+  function changeSlide(next = true) {
+    if (next) {
+      current += current > maxLeft ? -100 : current * -1;
+    } else {
+      current = current < 0 ? current + 100 : maxLeft;
+    }
+  
+    slides.style.left = current + "%";
+  }
+  
+  let autoChange = setInterval(changeSlide, delay);
+  const restart = function() {
+    clearInterval(autoChange);
+    autoChange = setInterval(changeSlide, delay);
+  };
+  
+  // Controls
+  document.querySelector(".next-slide").addEventListener("click", function() {
+    changeSlide();
+    restart();
+  });
+  
+  document.querySelector(".prev-slide").addEventListener("click", function() {
+    changeSlide(false);
+    restart();
+  });
+  
+
+
 //   gsap.registerPlugin(ScrollTrigger);
 // // REVEAL //
 // gsap.utils.toArray(".revealUp").forEach(function (elem) {
